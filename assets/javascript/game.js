d3e6d48ed414e99fc2bc7cd293fa60c983580eab
Sierra Kilo <sierrakilo@Sierras-MacBook-Pro.local>
2017-08-07 21:12:12 -0700

    game.js update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,18 +2,43 @@
 var randomNumber = null;
 var wins = 0;
 var losses = 0;
-var yourTotal = null;
+var yourTotal = 0;
 
-// crystals
+// crystal variables
 var crystalOne = null;
 var crystalTwo = null;
 var crystalThree = null;
 var crystalFour = null;
 
+// function to generate random number between 19-120
 function generateRandomNumber() {
     randomNumber = Math.floor(Math.random() * (121 - 18) + 18);
     $(".random-number").text(randomNumber);
-    return randomNumber
 }
 
-generateRandomNumber()
+// function to create random crystal number
+function crystalNumber() {
+    crystalOne = Math.floor(Math.random() * (19 - 1) + 1);
+    crystalTwo = Math.floor(Math.random() * (19 - 1) + 1);
+    crystalThree = Math.floor(Math.random() * (19 - 1) + 1);
+    crystalFour = Math.floor(Math.random() * (19 - 1) + 1);
+}
+
+// reset game function
+function reset() {
+    generateRandomNumber();
+    crystalNumber();
+    wins = 0;
+    losses = 0;
+    yourTotal = 0;
+}
+
+// update display function
+function updateDisplay() {
+
+}
+
+// gameplay onclick function
+document.onclick = function(event) {
+
+}
