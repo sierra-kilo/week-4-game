8df023cf1390ba69cd9881addb90540c2e63acb9
Sierra Kilo <sierrakilo@calvisitor-10-105-141-62.calvisitor.1918.berkeley.edu>
2017-08-08 20:57:02 -0700

    complete no images or styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,33 +12,53 @@ var crystalFour = null;
 
 // function to generate random number between 19-120
 function generateRandomNumber() {
-    randomNumber = Math.floor(Math.random() * (121 - 18) + 18);
-    $(".random-number").text(randomNumber);
+    randomNumber = Math.floor(Math.random() * (121 - 19) + 19);
 }
 
 // function to create random crystal number
 function crystalNumber() {
     crystalOne = Math.floor(Math.random() * (19 - 1) + 1);
+    $('#crystal-1').val(crystalOne);
     crystalTwo = Math.floor(Math.random() * (19 - 1) + 1);
+    $('#crystal-2').val(crystalTwo);
     crystalThree = Math.floor(Math.random() * (19 - 1) + 1);
+    $('#crystal-3').val(crystalThree);
     crystalFour = Math.floor(Math.random() * (19 - 1) + 1);
+    $('#crystal-4').val(crystalFour);
 }
 
 // reset game function
 function reset() {
     generateRandomNumber();
     crystalNumber();
-    wins = 0;
-    losses = 0;
     yourTotal = 0;
+
 }
 
 // update display function
 function updateDisplay() {
-
+    $(".random-number").text(randomNumber);
+    $('.win-total').text(wins);
+    $('.losses-total').text(losses);
+    $('.your-total').text(yourTotal);
 }
 
 // gameplay onclick function
-document.onclick = function(event) {
+$('.crystal').click(function () {
+    var crystalValue = this.value;
+    yourTotal += parseInt(crystalValue);
+    if (yourTotal > randomNumber) {
+        // you lose
+        losses++;
+        reset();
+    } else if (yourTotal === randomNumber) {
+        // you win
+        wins++;
+        reset();
+    }
+    updateDisplay();
+})
 
-}
+// start gameplay
+reset();
+updateDisplay();
